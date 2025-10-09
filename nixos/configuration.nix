0dfe9414f4f1af4a2bbebe619b94386a04299d65
sudosteak <devel9943@proton.me>
2025-10-09 14:11:02 -0400

    Add disko module and disk configuration; update services and user settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+      ./disk-config.nix
     ];
 
   # Bootloader.
@@ -20,67 +22,69 @@
   networking.hostName = "silverhand"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Toronto";
+  time.timeZone = "Canada/Eastern";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
   # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+      xkb.variant = "";
+    };
+    wayland = {
+      enable = true;
+    };
+  
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+    
+    # enable cups to print shit
+    printing.enable = true;
+    
+    # Enable virtualization
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+    # enable sound with pipewire
+    pulseaudio.enable = false;
+    rtkit.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      #jack.enable = true;
+      #media-session.enable = true;
+    };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # configure user accounts
   users.users.j = {
     isNormalUser = true;
     description = "jacob";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    initialHashedPassword = "$y$j9T$.TwhpvoNYVWYHSEjokQW/1$N/yMwStCdTMUkkjUVU2OKXk/I4h4LBAAGK0993EeRe/";
     packages = with pkgs; [
-
     ];
   };
 
+  # set root password
+  users.users.root = {
+    initialHashedPassword = "$y$j9T$CIyOpmbDlWt1SZl/8hEYq0$ZeBkU9Y1fHCISV8UkdJVDMsdGRlB9oUv4NFsiLml81.";
+  };
+
   # Install firefox.
-  programs.firefox.enable = true;
+  programs = {
+    firefox = {
+      enable = true;
+    };
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -88,8 +92,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
     neovim
     git
     curl
